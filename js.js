93ccee9b6284afac0013dd6f5e7cd7e744e68d15
Abhey Singh <abheys172@gmail.com>
2023-08-28 18:01:30 +0530

    D

--- js.js
+++ js.js
@@ -112,7 +112,7 @@ gsap.to(".centext1",{
     scrollTrigger:{
       trigger:"#page1",
       scrroller:"#main",
-      start:"top -53%",
+      start:"top -58%",
       end:"top -200%",
       scrub:true,
       pin:true,
@@ -214,7 +214,6 @@ gsap.to(".centext1",{
     scrollTrigger:{
       trigger:"#footer",
       scroller:"#main",
-      markers:true,
       start:"top 40%",end:"top -10%",scrub:true
     }
   })
